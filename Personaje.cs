853a55d992cccac8480fba3bae87bb94b3095d57
Sergio03 <mariano13martin@gmail.com>
2023-06-20 22:47:39 -0300

    GeneracionAleatoria

---
+++ Personaje.cs
@@ -0,0 +1,158 @@
+namespace EspacioPersonaje
+{
+    public class Personaje
+    {
+        // Datos
+        private string? tipo;
+        private string? nombre;
+        private string? apodo;
+        private DateTime fechaNac;
+        private int edad;
+        // Caracteristicas
+        private float velocidad;
+        private float destreza;
+        private float fuerza;
+        private int nivel;
+        private float armadura;
+        private float salud;
+        // 
+        public string? Tipo { get => tipo; set => tipo = value; }
+        public string? Nombre { get => nombre; set => nombre = value; }
+        public string? Apodo { get => apodo; set => apodo = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+        public int Edad { get => edad; set => edad = value; }
+        // 
+        public float Velocidad { get => velocidad; set => velocidad = value; }
+        public float Destreza { get => destreza; set => destreza = value; }
+        public float Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public float Armadura { get => armadura; set => armadura = value; }
+        public float Salud { get => salud; set => salud = value; }
+        // 
+        public void MostrarPersonajes(){
+            System.Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.Console.WriteLine("+ PERSONAJE: "+Nombre+" | APODO: "+Apodo);
+            System.Console.WriteLine("+ TIPO: "+Tipo+" | NIVEL: "+Nivel);
+            System.Console.WriteLine("+ EDAD: "+Edad+" | FECHA DE NACIMIENTO: "+FechaNac);
+            System.Console.WriteLine("+ CARACTERISTICAS:");
+            System.Console.WriteLine("--- VELOCIDAD: "+Velocidad);
+            System.Console.WriteLine("--- DESTREZA: "+Destreza);
+            System.Console.WriteLine("--- FUERZA: "+Fuerza);
+            System.Console.WriteLine("--- ARMADURA: "+Armadura);
+            System.Console.WriteLine("--- SALUD: "+Salud);
+        }
+    }
+
+    public class FabricaDePersonajes{
+        public string[] Nombres = {
+            // Guerreros
+            "King Arthur","Thorfinn Karlsefni","Aragorn","Thor","Askeladd","Leonidas","Kratos","Perseo","Levi Ackerman","Lancelot",
+            // Magos
+            "Gandalf","Sauron","Merlin","Doctor Strange","Voldemort","Shang Tsung","Morgana","Albus Dumbledore","Harry Potter","Sabio Oscuro",
+            // Arqueros
+            "Legolas","Hawkeye","Bardo I","Uryu Ishida","Green Arrow","Katnis Everdeen","Robin Hood","Susan Pevensie","Tauriel","Atreus",
+            // Cazadores
+            "Tanjiro Kamado","Zenitsu Agatsuma","Giyu Tomioka","Kyojuro Rengoku","Shinobu Kocho","Ichigo Kurosaki","Gon Freecss","Guts","Blade","Van Helsing",
+            // Demonios
+            "Muzan Kibutsuji","Akaza","Gyuutarou","Azazel","Astaroth","Lucifer","Lilith","Leviathan","Beelzebub","Susamaru",
+            // Criaturas
+            "Gollum","Fenrir","Griffin","Chimera","Centaur","Phoenix","Pegasus","Gorgon","Cerberus","Medusa"
+        };
+        public string[] Tipos = {
+            "Guerrero","Mago","Arquero","Cazador","Demonio","Criatura"
+        };
+        public string[] Apodos = {
+            // Guerreros
+            "El Destructor","El Indomable","El Filo Mortal","El Lobo de Guerra","El Azote","El Implacable","El Despiadado","El Señor de la Guerra","El Martillo de Hierro","El Guerrero de la Oscuridad",
+            // Magos
+            "El Hechicero","El Brujo Astral","El Oráculo","El Vidente de las Sombras","El Maestro de los Elementos","El Hechicero Supremo","El Encantador","El Mago Estelar","El Guardián de los Secretos","El Invocador",
+            // Arqueros
+            "El Ojo de Águila","El Francotirador","El Maestro del Arco","El Disparo Preciso","El Acechador Silencioso","El Arquero Veloz","El Arquero Mortal","El Lanzador de Flechas","El Vigía de los Bosques","El Verdugo con Arco",
+            // Cazadores
+            "El Rastreador","El Acechador","El Cazador Silencioso","El Maestro de la Presa","El Buscador de Trofeos","El Verdugo de Bestias","El Rastreador de Sombras","El Asesino Furtivo","El Vigía de la Naturaleza","El Devorador de Presas",
+            // Demonios
+            "El Devorador de Almas","El Desgarrador de Espíritus","El Lamento de la Oscuridad","El Portador de Tormentas","El Señor de las Sombras","El Destructor de Mundos","El Conquistador de Reinos","El Devastador Infernal","El Seraphim Oscuro","El Devorador de Universos",
+            // Criaturas
+            "El Errante Dimensional","El Ser de Mil Ojos","El Rugido de las Profundidades","El Señor de las Bestias","El Guardián de los Bosques","El Espíritu Errante","El Coloso de Piedra","El Vigía de las Cavernas","El Guardián de los Portales","El Embajador de la Oscuridad",
+        };
+
+        public Personaje CrearPersonaje(){
+            Personaje personaje = new Personaje();
+            Random aleatorio = new Random();
+            personaje.Tipo = Tipos[aleatorio.Next(0,6)];
+            personaje.Salud = (float)100;
+            switch (personaje.Tipo){
+            case "Guerrero":
+                personaje.Nombre = Nombres[aleatorio.Next(0,11)];
+                personaje.Apodo = Apodos[aleatorio.Next(0,11)];
+                personaje.Velocidad = (float)aleatorio.Next(1,11);
+                personaje.Destreza = (float)aleatorio.Next(1,6);
+                personaje.Fuerza = (float)aleatorio.Next(1,11);
+                personaje.Armadura = (float)aleatorio.Next(1,11);
+                break;
+            case "Mago":
+                personaje.Nombre = Nombres[aleatorio.Next(10,21)];
+                personaje.Apodo = Apodos[aleatorio.Next(10,21)];
+                personaje.Velocidad = (float)aleatorio.Next(1,11);
+                personaje.Destreza = (float)aleatorio.Next(1,6);
+                personaje.Fuerza = (float)aleatorio.Next(1,11);
+                personaje.Armadura = (float)aleatorio.Next(1,11);
+                break;
+            case "Arquero":
+                personaje.Nombre = Nombres[aleatorio.Next(20,31)];
+                personaje.Apodo = Apodos[aleatorio.Next(20,31)];
+                personaje.Velocidad = (float)aleatorio.Next(1,11);
+                personaje.Destreza = (float)aleatorio.Next(1,6);
+                personaje.Fuerza = (float)aleatorio.Next(1,11);
+                personaje.Armadura = (float)aleatorio.Next(1,11);
+                break;
+            case "Cazador":
+                personaje.Nombre = Nombres[aleatorio.Next(30,41)];
+                personaje.Apodo = Apodos[aleatorio.Next(30,41)];
+                personaje.Velocidad = (float)aleatorio.Next(1,11);
+                personaje.Destreza = (float)aleatorio.Next(1,6);
+                personaje.Fuerza = (float)aleatorio.Next(1,11);
+                personaje.Armadura = (float)aleatorio.Next(1,11);
+                break;
+            case "Demonio":
+                personaje.Nombre = Nombres[aleatorio.Next(40,51)];
+                personaje.Apodo = Apodos[aleatorio.Next(40,51)];
+                personaje.Velocidad = (float)aleatorio.Next(1,11);
+                personaje.Destreza = (float)aleatorio.Next(1,6);
+                personaje.Fuerza = (float)aleatorio.Next(1,11);
+                personaje.Armadura = (float)aleatorio.Next(1,11);
+                break;
+            case "Criatura":
+                personaje.Nombre = Nombres[aleatorio.Next(50,61)];
+                personaje.Apodo = Apodos[aleatorio.Next(50,61)];
+                personaje.Velocidad = (float)aleatorio.Next(1,11);
+                personaje.Destreza = (float)aleatorio.Next(1,6);
+                personaje.Fuerza = (float)aleatorio.Next(1,11);
+                personaje.Armadura = (float)aleatorio.Next(1,11);
+                break;
+            }
+            int mes = aleatorio.Next(1,13);
+            int dia;
+            switch (mes){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    dia = aleatorio.Next(1,32);
+                    break;
+                case 2:
+                    dia = aleatorio.Next(1,29);
+                    break;
+                default:
+                    dia = aleatorio.Next(1,31);
+                    break;
+            }
+            personaje.FechaNac = new DateTime(dia,mes,aleatorio.Next(1723,2024));
+            personaje.Edad = (int)(DateTime.Now.Year - personaje.FechaNac.Year);
+            return personaje;
+        }
+    }
+}
